c05fb48119e7d7543504f92501a175f70f63d9c9
Wes Lord <weslord@gmail.com>
2022-01-30 12:10:48 -0800

    Add debug level navigation

--- Assets/Scripts/ResetListener.cs
+++ Assets/Scripts/ResetListener.cs
@@ -4,7 +4,12 @@ using UnityEngine.SceneManagement;
 public class ResetListener : MonoBehaviour
 {
     void Awake() {
-        DontDestroyOnLoad(this);
+        ResetListener[] listeners = GameObject.FindObjectsOfType<ResetListener>();
+        if (listeners.Length > 1) {
+            Destroy(this.gameObject);
+        } else {
+            DontDestroyOnLoad(this);
+        }
     }
 
     void Update() {
@@ -13,10 +18,26 @@ public class ResetListener : MonoBehaviour
 
             if (currentScene.name == "Win Screen") {
                 // Restart entire game
-                SceneManager.LoadSceneAsync(0);
+                SceneManager.LoadScene(0);
             } else {
                 // Reset just this level
-                SceneManager.LoadSceneAsync(currentScene.buildIndex);
+                SceneManager.LoadScene(currentScene.buildIndex);
+            }
+        }
+
+        if (Input.GetKeyDown(".")) {
+            Scene currentScene = SceneManager.GetActiveScene();
+
+            if (currentScene.buildIndex < SceneManager.sceneCountInBuildSettings - 1) {
+                SceneManager.LoadScene(currentScene.buildIndex + 1);
+            }
+        }
+
+        if (Input.GetKeyDown(",")) {
+            Scene currentScene = SceneManager.GetActiveScene();
+
+            if (currentScene.buildIndex > 0) {
+                SceneManager.LoadScene(currentScene.buildIndex - 1);
             }
         }
     }
