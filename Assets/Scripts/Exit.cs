be126abf4a61fef9ad76861a0c53342accf04b01
Wes Lord <weslord@gmail.com>
2022-01-29 22:46:48 -0800

    Require multiple exit triggers to advance to the next level

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -3,8 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collider) {
+    static int exitCount = 0;
+    public int requiredExits = 2;
+
+    void LoadNextLevel() {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadSceneAsync(currentScene + 1);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        exitCount++;
+
+        if (exitCount == requiredExits ) {
+            LoadNextLevel();
+            exitCount = 0;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider) {
+        exitCount--;
     }
 }
