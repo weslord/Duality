02753c156e5c6999f787b6355498d174f199c356
Wes Lord <weslord@gmail.com>
2022-01-30 02:58:12 -0800

    Add (ineffective) implemetation of unidirectional controls

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class MainController : MonoBehaviour
 {
+    public OtherController follower;
+
     public float moveSpeed = 75f;
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void FixedUpdate() {
         float dx = Input.GetAxisRaw("Horizontal") * moveSpeed;
         float dy = Input.GetAxisRaw("Vertical") * moveSpeed;
+        Vector2 v = new Vector2(dx, dy);
 
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(dx, dy);
+        // NOTE: this does not work well.
+        follower.incomingVelocity = rb.velocity; // send old velocity out
+
+        rb.velocity = v;
     }
-
-
 }
--- Assets/Scripts/OtherController.cs
+++ Assets/Scripts/OtherController.cs
@@ -4,11 +4,13 @@ public class OtherController : MonoBehaviour
 {
     public float moveSpeed = 75f;
 
+    public Vector2 incomingVelocity = new Vector2(0, 0);
+
     void FixedUpdate() {
-        float dx = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        float dy = Input.GetAxisRaw("Vertical") * moveSpeed;
+        // float dx = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        // float dy = Input.GetAxisRaw("Vertical") * moveSpeed;
 
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(dx, dy);
+        rb.velocity = incomingVelocity;
     }
 }
